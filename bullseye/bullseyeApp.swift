c96abc44f3c8972640b286032e8eb7f5a23cbeda
Adam-A-AND <adam.armstrong@and.digital>
2022-08-23 16:43:18 +0100

    Initial commit

---
+++ bullseye/bullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyeApp.swift
+//  bullseye
+//
+//  Created by Adam Armstrong on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct bullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
