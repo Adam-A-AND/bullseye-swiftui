ce5d12579e1661724a0aa0d5ec95365d8ea77c0a
Adam-A-AND <adam.armstrong@and.digital>
2022-08-25 13:07:49 +0100

    feat(Task 4): Complete game UI

---
+++ bullseye/Views/BackgroundView.swift
@@ -0,0 +1,82 @@
+//
+//  BackgroundView.swift
+//  bullseye
+//
+//  Created by Adam Armstrong on 25/08/2022.
+//
+
+import SwiftUI
+
+struct BackgroundView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        VStack {
+            TopView(game: $game)
+            Spacer()
+            BottomView(game: $game)
+        }
+        .padding()
+        .background(
+            RingsView()
+        )
+    }
+}
+
+struct RingsView: View {
+    var body: some View {
+        ZStack {
+            Color("BackgroundColour")
+                .edgesIgnoringSafeArea(.all)
+            ForEach(1..<6) { ring in
+                let size = CGFloat(ring * 100)
+                Circle()
+                    .stroke(lineWidth: 20.0)
+                    .fill(RadialGradient(colors: [Color("RingColour").opacity(0.3 * 0.8), Color("RingColour").opacity(0)], center: .center, startRadius: 100, endRadius: 300))
+                    .frame(width: size, height: size)
+            }
+        }
+    }
+}
+
+struct TopView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            RoundedImageViewStroked(systemName: "arrow.counterclockwise")
+            Spacer()
+            RoundedImageViewFilled(systemName: "arrow.counterclockwise")
+        }
+    }
+}
+
+struct NumberView: View {
+    var title: String
+    var text: String
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            LabelText(text: title.uppercased())
+            RoundedRectTextView(text: text)
+        }
+    }
+}
+
+struct BottomView: View {
+    @Binding var game: Game
+    
+    var body: some View {
+        HStack {
+            NumberView(title: "Score", text: String(game.score))
+            Spacer()
+            NumberView(title: "Round", text: String(game.round))
+        }
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView(game: .constant(Game()))
+    }
+}
